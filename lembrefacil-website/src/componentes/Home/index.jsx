b6ecdb80bfc8e6924aa800c83b5ead5f598d4dd9
Igor Oliveira <igornutrietec2020@gmail.com>
2024-06-20 12:11:30 -0300

    Página Home iniciada

--- lembrefacil-website/src/componentes/Home/index.jsx
+++ lembrefacil-website/src/componentes/Home/index.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import logo from '../../assets/logo 1.png';
+import Carrossel from '../Carrossel';
 
 import './style.css';
 
@@ -6,8 +8,13 @@ function Home() {
     return (
         <>
         <main className='main-home'>
-            <section className=''>
-
+            <section className='main-home-content'>
+                <aside className='main-home-text'>
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolor labore vitae sed ipsa eveniet tempora voluptates vel? Voluptatem quasi quibusdam at quis, soluta error incidunt tenetur reprehenderit sunt, culpa a!</p>
+                </aside>
+                <aside className='main-home-image'>
+                    <Carrossel/>
+                </aside>
             </section>
         </main>
         </>
